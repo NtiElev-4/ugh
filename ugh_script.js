7ac30c8b259a33b0ed96f739f2eb8260e4929dcf
NtiElev-4 <156201535+NtiElev-4@users.noreply.github.com>
2024-01-17 14:03:50 +0100

    Update ugh_script.js

--- ugh_script.js
+++ ugh_script.js
@@ -1,1 +1,8 @@
 
+function number ad(){
+let nr1 = document.getElementById('nr1').value;
+let nr2 = document.getElementById('nr2').value;
+let nr3 = Number(nr1) + Number(nr2);
+document.getElementById('num').innerHTML = nr3;
+}
+
